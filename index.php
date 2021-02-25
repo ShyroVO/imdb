4342ab9d95e5052a5b26f28240b2f440bc164879
ShyroVO <Bosquet.laurie@outlook.com>
2021-02-25 14:43:47 +0100

    1

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./elements/style.css">
+    <link rel="stylesheet" href="./elements/cardStyle.css">
+</head>
+
+<body>
+
+    <nav>
+        <div id="headerLogo">LOGO</div>
+        <div id="headerMenu">Menu</div>
+        <div id="headerSearch">...</div>
+        <div>MIDBPRO</div>
+        <div>|</div>
+        <div>Watchlite</div>
+        <div>|</div>
+        <div>sing in</div>
+    </nav>
+
+    <div id="page">
+        <div id="ligne1">
+            <div class="container">
+                <div class="card">
+                    <h3 class="title">TEXT 1</h3>
+                    <div class="bar">
+                        <div class="emptybar"></div>
+                        <div class="filledbar"></div>
+                    </div>
+                    <div class="circle">
+                        Text text text text
+                    </div>
+                </div>
+                <div class="card">
+                    <h3 class="title">TEXT 2</h3>
+                    <div class="bar">
+                        <div class="emptybar"></div>
+                        <div class="filledbar"></div>
+                    </div>
+                    <div class="circle">
+                        Text text text text
+                    </div>
+                </div>
+                <div class="card">
+                    <h3 class="title">TEXT 3</h3>
+                    <div class="bar">
+                        <div class="emptybar"></div>
+                        <div class="filledbar"></div>
+                    </div>
+                    <div class="circle">
+                        Text text text text
+                    </div>
+                </div>
+                <div class="card">
+                    <h3 class="title">TEXT 4</h3>
+                    <div class="bar">
+                        <div class="emptybar"></div>
+                        <div class="filledbar"></div>
+                    </div>
+                    <div class="circle">
+                        Text text text text
+                    </div>
+                </div>
+            </div>
+            <div>TOP</div>
+        </div>
+
+<?php
+include "./elements/footer.php";
+?>
